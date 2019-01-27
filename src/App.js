4b6221c9849ddf10c6921fb18ddda2a5a4f249cc
James Priest <james-priest@outlook.com>
2019-01-27 12:31:17 -0800

    feat: Create UserInput component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: []
+  };
+  saveUser = user => {
+    console.log(user);
+    this.setState(prevState => ({
+      users: [...prevState.users, user]
+    }));
+  };
   render() {
     return (
       <div className="App">
@@ -13,7 +22,7 @@ class App extends Component {
         <main className="App-main">
           <div className="container">
             <h2>User Game List</h2>
-            <UserInput />
+            <UserInput saveUser={this.saveUser} />
             <UserList />
           </div>
         </main>
@@ -23,21 +32,71 @@ class App extends Component {
 }
 
 class UserInput extends Component {
+  state = {
+    fname: "",
+    lname: "",
+    username: ""
+  };
+  handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    // console.log(e.target.name);
+    this.setState({
+      [name]: value
+    });
+  };
+  fieldsAreValid = e => {
+    const { fname, lname, username } = this.state;
+    const valid = fname.length > 0 && lname.length > 0 && username.length > 0;
+    return valid;
+  };
+  addUser = e => {
+    e.preventDefault();
+    console.log("saving...");
+    // const { fname, lname, username } = this.state;
+    // this.props.saveUser({ fname, lname, username });
+    this.props.saveUser({ ...this.state });
+  };
   render() {
+    const { fname, lname, username } = this.state;
     return (
       <div>
         <h3>Add User</h3>
-        <form action="">
-          <label for="fname">First Name: </label>
-          <input id="fname" type="text" />
+        <form onSubmit={this.addUser}>
+          <label>
+            First Name:
+            <input
+              name="fname"
+              type="text"
+              value={fname}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
           <br />
-          <label for="lname">Last Name: </label>
-          <input id="lname" type="text" />
+          <label>
+            Last Name:
+            <input
+              name="lname"
+              type="text"
+              value={lname}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
           <br />
-          <label for="username">Username: </label>
-          <input id="username" type="text" />
+          <label>
+            Username:
+            <input
+              name="username"
+              type="text"
+              value={username}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
           <br />
-          <button disabled="true">Add</button>
+          <button disabled={!this.fieldsAreValid()}>Add</button>
         </form>
       </div>
     );
