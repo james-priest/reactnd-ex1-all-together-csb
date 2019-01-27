cc842b750142dc237876f3f06b2a185978148e9e
James Priest <james-priest@outlook.com>
2019-01-27 10:33:41 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="logo.svg" className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+          <p>Exercise 1 - All Together</p>
+        </header>
+        <main className="App-main">
+          <div className="container">
+            <p>[Template]</p>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+class UserInput extends Component {}
+
+export default App;
